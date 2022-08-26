619b1540b20426f7f76f8ce484efc712900e80b6
Thiago de Oliveira Albuquerque <thiago.work.TI@outlook.com>
2022-08-26 18:48:13 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+// Created by karnsugh on 26/08/22.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+    printf(" LED CALC v1.0 \n\n");
+    printf(" Autor: Eng. Thiago Albuquerque \n\n");
+
+    system("pause");
+    return 0;
+}
